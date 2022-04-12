bc2cca1062aa2f99bb910b57401e49d6f6dc7578
Anna Efimova <annaryabko@gmail.com>
2022-04-12 14:43:06 +0300

    verc
    git push
    anna@Annas-MacBook-Air mesto % git commit -m verc

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,17 +4,20 @@ let closeIcon = document.querySelector('.popup__close');
 let popupInputs = document.querySelector('.popup__inputs');
 let profileHeader = document.querySelector('.profile__header');
 let profileTitle = document.querySelector('.profile__title');
-let nameInput = document.querySelector('.popup__input-header');
-let jobInput = document.querySelector('.popup__input-title');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_description');
+let body = document.querySelector('body');
 
 function openPopup() {
   popup.style.display = "block";
   nameInput.value = profileHeader.innerText;
   jobInput.value = profileTitle.innerText;
+  body.style.overflow = 'hidden';
 }
 
 function closePopup() {
   popup.style.display = "none";
+  body.style.overflow = 'auto';
 }
 
 function formSubmitHandler(evt) {
